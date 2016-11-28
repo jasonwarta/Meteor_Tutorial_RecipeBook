da809835a05a3b9363103a1a53ca6b43a68c660e
Jason Warta <jasonwarta@gmail.com>
2016-11-28 13:12:14 -0900

    added password verification

--- client/accounts/Accounts.js
+++ client/accounts/Accounts.js
@@ -1,10 +1,23 @@
+AccountsTemplates.removeField('password');
 AccountsTemplates.addFields([
-{
-	_id: 'username',
-	type: 'text',
-	displayName: 'Username',
-	required: true
-}
+	{
+		_id: 'username',
+		type: 'text',
+		displayName: 'Username',
+		required: true
+	}
+	,
+	{
+	    _id: 'password',
+	    type: 'password',
+	    placeholder: {
+	        signUp: "Password"
+	    },
+	    required: true,
+	    minLength: 8,
+	    re: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*-_=+]).{8,}/,
+	    errStr: 'At least 1 digit, 1 lowercase, and 1 uppercase, and 1 of \"!@#$%^&*-_=+\"',
+	}
 ]);
 
 AccountsTemplates.configure({
