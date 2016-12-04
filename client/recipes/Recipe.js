1f6a570ac767c222e82cb1f1cffe72799f4f9474
Jason Warta <jasonwarta@gmail.com>
2016-12-03 16:24:47 -0900

    added fav button to MyRecipes page

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -8,6 +8,11 @@ Template.Recipe.helpers({
 	},
 	editMode: function() {
 		return Template.instance().editMode.get();
+	},
+	favorite: function(id) {
+		var favs = Recipes.findOne(id).favorites;
+		if(typeof(favs) !== 'undefined')
+			return favs.includes(Meteor.userId())
 	}
 });
 
@@ -20,5 +25,11 @@ Template.Recipe.events({
 	},
 	'submit': function(event, template){
 		template.editMode.set(false);
+	},
+	'click .fa-heart' : function(event, template) {
+		Meteor.call('removeFavorite',template.data._id)
+	},
+	'click .fa-heart-o' : function(event, template) {
+		Meteor.call('setFavorite',template.data._id)
 	}
 });
